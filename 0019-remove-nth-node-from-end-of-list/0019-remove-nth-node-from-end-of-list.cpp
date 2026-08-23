eba98cb3003031333ed7ab39cc96864a22666c79
Ankush Kumar <118971600+ankushkumar01@users.noreply.github.com>
2026-08-23 18:09:39 +0530

    0 ms (100%) 15 MB (59.71%) 0019-remove-nth-node-from-end-of-list .cpp Medium

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -2,24 +2,42 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        ListNode* temp= head;
-        int len=0;
-        while (temp != nullptr){
-            len++;
-            temp=temp->next;
+        ListNode *slow=head;
+        ListNode *fast=head;
+
+        for (int i=1; i<=n+1; i++){
+            if(fast==nullptr)return head->next;
+            fast=fast->next;
         }
 
-        if (len == n){
-            head= head->next;
-            return head;
+        while(fast != nullptr){
+            fast=fast->next;
+            slow=slow->next;
         }
-        else{
-            temp=head;
-            for (int i=1; i<len-n; i++){
-                temp=temp->next;
-            }
-            temp->next=temp->next->next;
-            return head;
-        }
+
+        slow->next= slow->next->next;
+        return head;
+
+
+
+        // ListNode* temp= head;
+        // int len=0;
+        // while (temp != nullptr){
+        //     len++;
+        //     temp=temp->next;
+        // }
+
+        // if (len == n){
+        //     head= head->next;
+        //     return head;
+        // }
+        // else{
+        //     temp=head;
+        //     for (int i=1; i<len-n; i++){
+        //         temp=temp->next;
+        //     }
+        //     temp->next=temp->next->next;
+        //     return head;
+        // }
     }
 };
