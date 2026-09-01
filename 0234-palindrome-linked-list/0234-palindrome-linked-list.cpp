424f99d31b55f00e15185ac86582296a7b68d012
Ankush Kumar <118971600+ankushkumar01@users.noreply.github.com>
2026-09-01 19:23:03 +0530

    0 ms (100%) 122 MB (39.35%) 0234-palindrome-linked-list .cpp Easy

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -1,32 +1,35 @@
 class Solution {
 public:
-    ListNode* reverse(ListNode* head) {
-        if (head == nullptr || head->next == nullptr)
-            return head;
-        ListNode* newhead = reverse(head->next);
-        head->next->next = head;
-        head->next = nullptr;
-        return newhead;
+    ListNode* reverse ( ListNode * head){
+        ListNode * prev= nullptr;
+        ListNode * curr= head;
+        ListNode * nex = head;
+        while ( curr){
+            nex= curr->next;
+            curr->next= prev;
+            prev= curr;
+            curr= nex;
+        }
+        return prev;
     }
     bool isPalindrome(ListNode* head) {
-        ListNode* c = new ListNode (0);
-        ListNode* temp= head;
-        ListNode* temp1= c;
-        while ( temp != nullptr){
-            ListNode* node = new ListNode (temp->val);
-            temp1->next= node;
-            temp= temp->next;
-            temp1 = temp1->next;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast->next != nullptr && fast->next->next != nullptr) {
+            slow= slow->next;
+            fast= fast->next->next;
         }
-        c=c->next;
-        
-        ListNode* rev = reverse(c);
-        while (head) {
-            if ( head->val != rev->val ){
+        ListNode * rev= reverse( slow->next);
+        slow->next= nullptr;
+        slow-> next= rev;
+        slow= slow->next;
+
+        while ( slow){
+            if ( head->val != slow->val){
                 return false;
             }
             head= head->next;
-            rev=rev->next;
+            slow= slow->next;
         }
         return true;
     }
